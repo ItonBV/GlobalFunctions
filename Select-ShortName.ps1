e2561ef645b318bfd45db26f7a84c925f93aa5fd
Roy Spekreijse <roy@spekreijse.eu>
2018-04-12 11:03:40 +0200

    Create Select-ShortName.ps1

---
+++ Select-ShortName.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    Example of how to use this cmdlet
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+function Select-ShortName {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $LongName
+    )
+
+    begin {
+    }
+
+    process {
+        if ($LongName.Length > 6) {
+            $ShortName = $LongName.Substring(0, 5)
+        }
+        else {
+            $ShortName = $LongName
+        }
+        $ShortName
+    }
+
+    end {
+    }
+}
